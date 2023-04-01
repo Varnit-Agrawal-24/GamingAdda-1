51efc2eb91debcd10cae2285e2dd95b65d590d2c
Trinay Bhati <bhatiji780780@gmail.com>
2023-04-01 11:46:13 +0530

    1-apr

--- js sliding game/script.js
+++ js sliding game/script.js
@@ -1,39 +1,38 @@
-var count = 0;
+let count = 0;
 
-function stopSliding(){
-    count++
-    var slid = "slider" + count;
-    var sliderCurrent = document.getElementById(slid);
-    
-    var slid = "slider" + (count + 1)
-    var sliderAbove = document.getElementById(slid);
+function stopSliding() {
+  count++;
+  let slid = "slider" + count;
+  let sliderCurrent = document.getElementById(slid);
 
-    if(count == 1){
-        var sliderBelow = sliderCurrent
-    }else{
-        var slid = "slider" + (count - 1)
-    var sliderBelow = document.getElementById(slid)
-    }
+  slid = "slider" + (count + 1);
+  let sliderAbove = document.getElementById(slid);
 
-    var left = parseInt(window.getComputedStyle(sliderCurrent).getPropertyValue("left"));
-    var width = parseInt(window.getComputedStyle(sliderCurrent).getPropertyValue("width"));
-    var leftBelow = parseInt(window.getComputedStyle(sliderBelow).getPropertyValue("left"))
+  let sliderBelow;
+  if (count == 1) {
+    sliderBelow = sliderCurrent;
+  } else {
+    slid = "slider" + (count - 1);
+    sliderBelow = document.getElementById(slid);
+  }
+
+  let left = parseInt(window.getComputedStyle(sliderCurrent).getPropertyValue("left"));
+  let width = parseInt(window.getComputedStyle(sliderCurrent).getPropertyValue("width"));
+  let leftBelow = parseInt(window.getComputedStyle(sliderBelow).getPropertyValue("left"));
    
+  sliderCurrent.classList.remove('animate');
+  sliderCurrent.style.left = left + "px";
 
-    sliderCurrent.classList.remove('animate');
-    sliderCurrent.style.left = left + "px";
+  let diffrence = left - leftBelow;
+  let absDiffrence = Math.abs(diffrence);
+  let newWidth = (width - absDiffrence).toString().concat("px");
+  sliderCurrent.style.width = newWidth;
+  sliderAbove.style.width = newWidth;
+  sliderAbove.style.visibility = 'visible';
 
-    var diffrence = left - leftBelow;
-    var absDiffrence = Math.abs(diffrence);
-    var newWidth = (width - absDiffrence).toString().concat("px");
-    sliderCurrent.style.width = newWidth;
-    sliderAbove.style.width = newWidth;
-    sliderAbove.style.visibility = 'visible';
-
-    if(diffrence > width  || diffrence<-width){
-        var score = "score:" + count
-        alert("Your score is: " + score)
-        location.reload()
-    }
-
+  if (diffrence > width || diffrence < -width) {
+    let score =  count;
+    alert("Your score is : " + score);
+    location.reload();
+  }
 }
